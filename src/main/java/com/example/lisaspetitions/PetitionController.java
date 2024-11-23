f5cc396228d475a3a0e6cf8a81c28717cadf9b63
LisaR90 <lisaruffley90@gmail.com>
2024-11-23 19:24:41 +0000

    "Added a method to display all petitions and added some sample petitions, also created the petitions.html file"

--- src/main/java/com/example/lisaspetitions/PetitionController.java
+++ src/main/java/com/example/lisaspetitions/PetitionController.java
@@ -1,6 +1,7 @@
 package com.example.lisaspetitions;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,21 @@ import java.util.List;
 @Controller
 public class PetitionController {
     private List<Petition> petitions = new ArrayList<>();
+
+    // Sample data for testing
+    public PetitionController() {
+        petitions.add(new Petition("1", "Save the Forest", "Help protect our forests!", "Lisa"));
+        petitions.add(new Petition("2", "Clean the Oceans", "Reduce plastic waste in oceans", "Steve"));
+        petitions.add(new Petition("3", "Animal Rights", "Support animal protection laws", "Lily"));
+    }
+
+    // View all petitions
+    @GetMapping("/petitions")
+    public String viewAllPetitions(Model model) {
+        model.addAttribute("petitions", petitions);
+        return "petitions.html"; // Explicitly specify the file extension
+    }
+
 
     // Create a new petition page
     @GetMapping("/create")

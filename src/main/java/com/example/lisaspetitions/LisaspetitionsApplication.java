135d9ee98786d276be4ea6cbbf456e057493ab08
LisaR90 <lisaruffley90@gmail.com>
2024-11-23 19:12:21 +0000

    Initial commit with SpringBoot starter

---
+++ src/main/java/com/example/lisaspetitions/LisaspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lisaspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisaspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisaspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lisaspetitions/LisaspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lisaspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisaspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
